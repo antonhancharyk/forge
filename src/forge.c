669c19d18adb7303ae0778974ce4636e9e7e1010
antongoncharik <ant.goncharik@gmail.com>
2025-08-01 00:13:18 +0300

    Remove one line

--- src/forge.c
+++ src/forge.c
@@ -7,8 +7,6 @@
 void run_forge(const char *template_name, const char *output_name, char *vars[], int var_count)
 {
     char template_path[256];
-    snprintf(template_path, sizeof(template_path), "templates/%s.tpl", template_name);
-
     get_template_path(template_name, template_path, sizeof(template_path));
 
     if (create_file_from_template(template_path, output_name, vars, var_count) == 0)
